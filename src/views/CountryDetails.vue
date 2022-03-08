aebb6644b9f09d4b3b3e942d60c553e8008fa0d0
Victor <abidoye8224@gmail.com>
2022-03-08 23:50:20 +0100

    added first set of countryDetails info into component

--- src/views/CountryDetails.vue
+++ src/views/CountryDetails.vue
@@ -2,8 +2,50 @@
   <div class="p-5 bg-little-200 dark:bg-prudent-200">
     <CustomButton />
     <div>
-      <div>
-        <img :src="country.flags.svg" alt="">
+      <div class="w-80 h-56 mx-auto">
+        <img
+          :src="country.flags.svg"
+          alt=""
+          class="w-full h-full object-cover"
+        />
+      </div>
+      <div class="dark:text-white">
+        <h3 class="py-6 font-bold text-xl">{{ country.name }}</h3>
+        <div>
+
+        <div class="py-6">
+          <p class="font-medium text-sm py-1">
+            Native Name:
+            <span class="font-normal">{{ country.nativeName }}</span>
+          </p>
+          <p class="font-medium text-sm py-1">
+            Population:
+            <span class="font-normal">{{ country.population }}</span>
+          </p>
+          <p class="font-medium text-sm py-1">
+            Region: <span class="font-normal">{{ country.region }}</span>
+          </p>
+          <p class="font-medium text-sm py-1">
+            Sub Region: <span class="font-normal">{{ country.subregion }}</span>
+          </p>
+          <p class="font-medium text-sm py-1">
+            Capital: <span class="font-normal">{{ country.capital }}</span>
+          </p>
+        </div>
+        <div>
+          <p class="font-medium text-sm py-1">
+            Top Level Domain:
+            <span class="font-normal">{{ country.topLevelDomain[0] }}</span>
+          </p>
+          <p class="font-medium text-sm py-1">
+            Currencies:
+            <span class="font-normal">{{ country.currencies.name }}</span>
+          </p>
+          <p class="font-medium text-sm py-1">
+            Languages: <span class="font-normal">{{ country.region }}</span>
+          </p>
+        </div>
+        </div>
       </div>
     </div>
   </div>
